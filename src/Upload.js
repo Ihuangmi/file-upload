8cb62efb439151ca244eb0a112677ae769529761
huangmi <huangmi@newrank.cn>
2023-09-04 15:01:38 +0800

    feat: tijiao

--- src/Upload.js
+++ src/Upload.js
@@ -1,39 +1,42 @@
-import React, { useRef, useEffect, useReducer } from 'react'
-import { Button, Progress, message } from 'antd';
-import { SlideDown } from 'react-slidedown'
-import SparkMD5 from 'spark-md5'
-import 'react-slidedown/lib/slidedown.css'
-import './style.css'
-import axios from 'axios'
+import React, { useRef, useEffect, useReducer } from "react";
+import { Button, Progress, message } from "antd";
+import { SlideDown } from "react-slidedown";
+import SparkMD5 from "spark-md5";
+import "react-slidedown/lib/slidedown.css";
+import "./style.css";
+import axios from "axios";
 
-const BaseUrl = 'http://localhost:1111'
+const BaseUrl = "http://localhost:1111";
 
 const initialState = { checkPercent: 0, uploadPercent: 0 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'check':
-      initialState.checkPercent = action.checkPercent
-      return { ...initialState }
-    case 'upload':
-      initialState.uploadPercent = action.uploadPercent
-      return { ...initialState }
-    case 'init':
-      initialState.checkPercent = 0
-      initialState.uploadPercent = 0
-      return { ...initialState }
+    case "check":
+      initialState.checkPercent = action.checkPercent;
+      return { ...initialState };
+    case "upload":
+      initialState.uploadPercent = action.uploadPercent;
+      return { ...initialState };
+    case "init":
+      initialState.checkPercent = 0;
+      initialState.uploadPercent = 0;
+      return { ...initialState };
     default:
-      return { checkPercent: state.checkPercent, uploadPercent: state.uploadPercent }
+      return {
+        checkPercent: state.checkPercent,
+        uploadPercent: state.uploadPercent,
+      };
   }
 }
 
 const Upload = () => {
-  const [state, dispatch] = useReducer(reducer, initialState)
-  const inputRef = useRef(null)
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const inputRef = useRef(null);
   const chunks = 100; // 切成100份
-  const chunkSize = 5 * 1024 * 1024 // 切片大小
+  const chunkSize = 5 * 1024 * 1024; // 切片大小
   let checkCurrentChunk = 0; // 检查，当前切片
-  let uploadCurrentChunk = 0 // 上传，当前切片
+  let uploadCurrentChunk = 0; // 上传，当前切片
 
   /**
    * 将文件转换成md5并进行切片
@@ -42,169 +45,202 @@ const Upload = () => {
   const md5File = (file) => {
     return new Promise((resolve, reject) => {
       // 文件截取
-      let blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice,
+      let blobSlice =
+          File.prototype.slice ||
+          File.prototype.mozSlice ||
+          File.prototype.webkitSlice,
         chunkSize = file?.size / 100,
         spark = new SparkMD5.ArrayBuffer(),
         fileReader = new FileReader();
 
       fileReader.onload = function (e) {
-        console.log('read chunk nr', checkCurrentChunk + 1, 'of', chunks);
+        console.log("read chunk nr", checkCurrentChunk + 1, "of", chunks);
         spark.append(e.target.result);
         checkCurrentChunk += 1;
 
         if (checkCurrentChunk < chunks) {
           loadNext();
         } else {
-          let result = spark.end()
-          resolve(result)
+          let result = spark.end();
+          resolve(result);
         }
       };
 
       fileReader.onerror = function () {
-        message.error('文件读取错误')
+        message.error("文件读取错误");
       };
 
       const loadNext = () => {
         const start = checkCurrentChunk * chunkSize,
-          end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
+          end = start + chunkSize >= file.size ? file.size : start + chunkSize;
 
         // 文件切片
         fileReader.readAsArrayBuffer(blobSlice.call(file, start, end));
         // 检查进度条
-        dispatch({ type: 'check', checkPercent: checkCurrentChunk + 1 })
-      }
+        dispatch({ type: "check", checkPercent: checkCurrentChunk + 1 });
+      };
 
       loadNext();
-    })
-    
-  }
+    });
+  };
 
   /**
    * 校验文件
    * @param {*} fileName 文件名
    * @param {*} fileMd5Value md5文件
-   * @returns 
+   * @returns
    */
   const checkFileMD5 = (fileName, fileMd5Value) => {
-    let url = BaseUrl + '/check/file?fileName=' + fileName + "&fileMd5Value=" + fileMd5Value
-    return axios.get(url)
-  }
+    let url =
+      BaseUrl +
+      "/check/file?fileName=" +
+      fileName +
+      "&fileMd5Value=" +
+      fileMd5Value;
+    return axios.get(url);
+  };
 
   // 上传chunk
   function upload({ i, file, fileMd5Value, chunks }) {
-    uploadCurrentChunk = 0
+    uploadCurrentChunk = 0;
     //构造一个表单，FormData是HTML5新增的
-    let end = (i + 1) * chunkSize >= file.size ? file.size : (i + 1) * chunkSize
-    let form = new FormData()
-    form.append("data", file.slice(i * chunkSize, end)) //file对象的slice方法用于切出文件的一部分
-    form.append("total", chunks) //总片数
-    form.append("index", i) //当前是第几片     
-    form.append("fileMd5Value", fileMd5Value)
+    let end =
+      (i + 1) * chunkSize >= file.size ? file.size : (i + 1) * chunkSize;
+    let form = new FormData();
+    form.append("data", file.slice(i * chunkSize, end)); //file对象的slice方法用于切出文件的一部分
+    form.append("total", chunks); //总片数
+    form.append("index", i); //当前是第几片
+    form.append("fileMd5Value", fileMd5Value);
+
     return axios({
-      method: 'post',
+      method: "post",
       url: BaseUrl + "/upload",
-      data: form
+      data: form,
     }).then(({ data }) => {
       if (data.stat) {
-        uploadCurrentChunk = uploadCurrentChunk + 1
-        const uploadPercent = Math.ceil((uploadCurrentChunk / chunks) * 100)
-        dispatch({ type: 'upload', uploadPercent })
+        uploadCurrentChunk = uploadCurrentChunk + 1;
+        const uploadPercent = Math.ceil((uploadCurrentChunk / chunks) * 100);
+        dispatch({ type: "upload", uploadPercent });
       }
-    })
+    });
   }
 
   /**
    * 上传chunk
-   * @param {*} fileMd5Value 
-   * @param {*} chunkList 
+   * @param {*} fileMd5Value
+   * @param {*} chunkList
    */
   async function checkAndUploadChunk(file, fileMd5Value, chunkList) {
-    let chunks = Math.ceil(file.size / chunkSize)
-    const requestList = []
+    let chunks = Math.ceil(file.size / chunkSize);
+    console.log(
+      "🚀 ~ file: Upload.jsx:135 ~ checkAndUploadChunk ~ chunks:",
+      chunks
+    );
+    const requestList = [];
+
     for (let i = 0; i < chunks; i++) {
-      let exit = chunkList.indexOf(i + "") > -1
+      let exit = chunkList.indexOf(i + "") > -1;
       // 如果不存在，则上传
       if (!exit) {
-        requestList.push(upload({ i, file, fileMd5Value, chunks }))
+        requestList.push(upload({ i, file, fileMd5Value, chunks }));
       }
     }
 
     // 并发上传
     if (requestList?.length) {
-      await Promise.all(requestList)
+      await Promise.all(requestList);
     }
   }
 
   const responseChange = async (file) => {
     // 1.校验文件，返回md5
-    const fileMd5Value = await md5File(file)
+    const fileMd5Value = await md5File(file);
     // 2.校验文件的md5
-    const { data } = await checkFileMD5(file.name, fileMd5Value)
+    const { data } = await checkFileMD5(file.name, fileMd5Value);
     // 如果文件已存在, 就秒传
     if (data?.file) {
-      message.success('文件已秒传')
-      return
+      message.success("文件已秒传");
+      return;
     }
     // 3：检查并上传切片
-    await checkAndUploadChunk(file, fileMd5Value, data.chunkList)
+    await checkAndUploadChunk(file, fileMd5Value, data.chunkList);
     // 4：通知服务器所有服务器分片已经上传完成
-    notifyServer(file, fileMd5Value)
-  }
+    notifyServer(file, fileMd5Value);
+  };
 
   /**
    * 所有的分片上传完成，准备合成
-   * @param {*} file 
-   * @param {*} fileMd5Value 
+   * @param {*} file
+   * @param {*} fileMd5Value
    */
   function notifyServer(file, fileMd5Value) {
-    let url = BaseUrl + '/merge?md5=' + fileMd5Value + "&fileName=" + file.name + "&size=" + file.size
+    let url =
+      BaseUrl +
+      "/merge?md5=" +
+      fileMd5Value +
+      "&fileName=" +
+      file.name +
+      "&size=" +
+      file.size;
     axios.get(url).then(({ data }) => {
       if (data.stat) {
-        message.success('上传成功')
+        message.success("上传成功");
       } else {
-        message.error('上传失败')
+        message.error("上传失败");
       }
-    })
+    });
   }
 
-  useEffect(() => {
-    const changeFile = ({ target }) => {
-      dispatch({ type: 'init' })
-      const file = target.files[0]
-      responseChange(file)
-    }
+  const changeFile = ({ target }) => {
+    dispatch({ type: "init" });
+    const file = target.files[0];
+    responseChange(file);
+  };
 
-    document.addEventListener("change", changeFile)
+  // useEffect(() => {
+  //   const changeFile = ({ target }) => {
+  //     dispatch({ type: "init" });
+  //     const file = target.files[0];
+  //     responseChange(file);
+  //   };
 
-    return () => {
-      document.removeEventListener("change", changeFile)
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  //   document.addEventListener("change", changeFile);
+
+  //   return () => {
+  //     document.removeEventListener("change", changeFile);
+  //   };
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   return (
     <div className="wrap">
       <div className="upload">
         <span>点击上传文件：</span>
-        <input ref={inputRef} type="file" id="file" />
-        <Button type="primary" onClick={() => inputRef.current.click()}>上传</Button>
+        <input ref={inputRef} type="file" id="file" onChange={changeFile} />
+        <Button type="primary" onClick={() => inputRef.current.click()}>
+          上传
+        </Button>
       </div>
       {state.checkPercent > 0 && (
-        <SlideDown className={'my-dropdown-slidedown'} >
+        <SlideDown className={"my-dropdown-slidedown"}>
           <div className="uploading">
-            <div>校验文件进度：<Progress style={{ width: 200 }} percent={state.checkPercent} /></div>
+            <div>
+              校验文件进度：
+              <Progress style={{ width: 200 }} percent={state.checkPercent} />
+            </div>
           </div>
         </SlideDown>
       )}
       {state.uploadPercent > 0 && (
-        <SlideDown className={'my-dropdown-slidedown'} >
+        <SlideDown className={"my-dropdown-slidedown"}>
           <div className="uploading">
-            上传文件进度：<Progress type="circle" percent={state.uploadPercent} />
+            上传文件进度：
+            <Progress type="circle" percent={state.uploadPercent} />
           </div>
         </SlideDown>
       )}
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default Upload
+export default Upload;
